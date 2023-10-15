d2b2bdc38c7f9e5fcea8cdc2443835f1d1f64a66
Rokas <109721349+archlich03@users.noreply.github.com>
2023-10-15 11:15:59 +0300

    Added to the verbosity of `readjson.py`

--- scripts/readjson.py
+++ scripts/readjson.py
@@ -7,29 +7,31 @@ def write_data():
         json.dump(data, json_file, indent=4)
 
 file = open('job.json')
+timestamp = time.time()
 data = json.load(file)
 
 lastScan = data['lastScan']
 
 kept_data = []
 
+print(f"[{timestamp}] Beginning the reading of all inserted entries.")
 for entry in data['data']:
     url = entry['url']
     frequency = entry['frequency']
     lastDownload = entry['lastDownload']
 
     if(frequency == -1):
-        print(f"Removing {url}")
+        print(f"[{timestamp}] Removing {url} (frequency = -1).")
     else:
         if (lastDownload + frequency * 3600 <= lastScan):
-            print(f"Downloading {url}")
-            os.system(f"./spotdl-4.2.0-linux download {url}")
+            print(f"[{timestamp}] Downloading {url}")
+            os.system(f"./spotdl-4.2.0-linux download {url}.")
 
             entry['lastDownload'] = int(time.time())
             write_data()
-            print(f"{url} downloaded")
+            print(f"[{timestamp}] {url} finished downloading.")
         else:
-            print(f"Skipping {url}")
+            print(f"[{timestamp}] Skipping {url} due to the refresh time coming later.")
 
         kept_data.append(entry)
     
@@ -38,3 +40,4 @@ data['lastScan'] = int(time.time())
 
 write_data()
 file.close()
+print(f"[{timestamp}] Finished reading all inserted entries.")
