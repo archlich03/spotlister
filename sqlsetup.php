d9c4f60e9c33c29769a32d322eb87a3c7a79e604
nitrogency <nitrog@protonmail.com>
2023-11-22 17:15:27 +0200

    Updated tables

--- sqlsetup.php
+++ sqlsetup.php
@@ -17,7 +17,8 @@ function setupDB() {
         id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         url VARCHAR(255) NOT NULL,
         frequency INT(15) NOT NULL,
-        lastDownload INT(15) NOT NULL
+        lastDownload INT(15) NOT NULL,
+        user_id INT(6) UNSIGNED NOT NULL
     )
     ";
     
@@ -26,8 +27,17 @@ function setupDB() {
         lastScan INT(15) NOT NULL
     )
     ";
+
+    $sqlUsers = "
+    CREATE TABLE IF NOT EXISTS Users (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        approved BOOLEAN NOT NULL
+    )
+    ";
     
-    if ($conn->query($sqlPlaylists) === TRUE && $conn->query($sqlScanData) === TRUE) {
+    if ($conn->query($sqlPlaylists) === TRUE && $conn->query($sqlScanData) === TRUE && $conn->query($sqlUsers) === TRUE) {
         echo "\nTables created successfully";
     } else {
         die ("<h1>Error creating table: </h1>". $conn->error);
